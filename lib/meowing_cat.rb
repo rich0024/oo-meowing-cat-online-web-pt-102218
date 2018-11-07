2c4d01f5e5a7261389af315c7b85319c3594f8f1
Richard Yance <yance0022@gmail.com>
2018-11-07 20:53:34 +0000

    automatically backed up by learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,4 +1,17 @@
-## code your solution here. 
+## code your solution here.
 class Cat
   
+  def cat_attributes(props)
+    props.each do |prop|
+      define_method(:"@#{prop}") do
+          instance_variable_set(:"@#{prop}")
+      end
+    end
+    define_method(:"@#{prop}=") do |atr|
+      instance_variable_set(:"@#{prop}", atr)
+    end
+  end
+end  
+
+cat_attributes([:name, :meow])
 end
